05042c9af3e7fac4a4e32be84f8274be9fe8cb3a
Luis García <luis_grdz@hotmail.com>
2019-09-15 13:58:02 -0500

    Eliminación de código que no se necesitará e importación del componente buscador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Buscador from './componentes/Buscador'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // className se utiliza en javascript para dar estilos, ya que class es una palabra reservada del lenguaje
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de imágenes</p>
+        
+        <Buscador />
+      </div>
     </div>
   );
 }
