0e69c653813fdd44dec71ff79e2015735466fda2
Luis García <luis_grdz@hotmail.com>
2019-09-15 16:17:56 -0500

    Obtener y pasar datos entre componentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Buscador from './componentes/Buscador'
 
-function App() {
-  return (
-    // className se utiliza en javascript para dar estilos, ya que class es una palabra reservada del lenguaje
-    <div className="container">
-      <div className="jumbotron">
-        <p className="lead text-center">Buscador de imágenes</p>
-        
-        <Buscador />
+class App extends Component {
+  
+  datosBusqueda = (termino) => {
+    console.log('Desde APP ' + termino);
+  }
+
+  render() {
+    return (
+      // className se utiliza en javascript para dar estilos, ya que class es una palabra reservada del lenguaje
+      <div className="container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imágenes</p>
+
+          <Buscador 
+            datosBusqueda={ this.datosBusqueda }
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
 class Buscador extends Component {
+    
+    // createRef() permite leer los valores de los inputs con react
+    busquedaRef = React.createRef();
+
+    // funcion para obtener datos
+    obtenerDatos = (e) => {
+        e.preventDefault();
+        console.log('Desde el buscador ' + this.busquedaRef.current.value);
+
+        // Tomamos el valor del input
+        const termino = this.busquedaRef.current.value;
+        
+        // Se envía el valor del input al componente principal
+        this.props.datosBusqueda(termino);
+    }
+    
     render() {
         return (
-            <form>
+            //el evento onSubmit pertenece a react y es para mandar los datos al darle clic al boton submit
+            <form onSubmit={ this.obtenerDatos }>
                 <div className="row">
+                    {/* 
+                        con el props se reciben los datos desde otro componente 
+                        solo se pueden pasar props del componente principal al hijo
+                    */}
+                    {/* this.props.mensaje */}
                     <div className="form-group col-md-8">
-                        <input  type="text" 
+                        <input  ref={ this.busquedaRef }
+                                type="text" 
                                 className="form-control form-control-lg"
                                 placeholder="Busca tu imagen..." 
+                        />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <input  type="submit" 
+                                className="btn btn-lg btn-danger btn-block"
+                                value="Buscar" 
                         />
                     </div>
                 </div>
