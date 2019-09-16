988f3aa8b2c9a010a4d1105e2acc4c2e2810fdcf
Luis García <luis_grdz@hotmail.com>
2019-09-16 00:12:56 -0500

    Updates

---
+++ precache-manifest.e3d7566343c91c89eb31e945e5dd2912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27720fc3acf23fd6ec87f4cbd8abc0a2",
+    "url": "/reactpixabay/index.html"
+  },
+  {
+    "revision": "960bdfe49010460cbd24",
+    "url": "/reactpixabay/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3770c4ff4f27f3158580",
+    "url": "/reactpixabay/static/js/2.d9338b06.chunk.js"
+  },
+  {
+    "revision": "960bdfe49010460cbd24",
+    "url": "/reactpixabay/static/js/main.68883421.chunk.js"
+  },
+  {
+    "revision": "e6374a81f95c2cfa7a22",
+    "url": "/reactpixabay/static/js/runtime~main.052636ea.js"
+  }
+]);
