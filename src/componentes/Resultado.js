95889a95c20f0422422653e893b9ea1923bcbd4d
Luis García <luis_grdz@hotmail.com>
2019-09-15 20:45:32 -0500

    Creación y uso del componente imagen

--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Imagen from './Imagen';
 
 class Resultado extends Component {
 
@@ -13,11 +14,14 @@ class Resultado extends Component {
         console.log( imagenesRecibidas );
 
         return (
-            <React.fragment>
-                <div className="col-12 p-5 row">
-                    
-                </div>
-            </React.fragment>
+            <div className="col-12 p-5 row">
+                { imagenesRecibidas.map( imagenRecibida => (
+                    <Imagen 
+                        key = { imagenRecibida.id }
+                        imagen = { imagenRecibida }
+                    />
+                ))}
+            </div>
         );
         
     }
